826f49145fa6335e97271c7d0079a7df0e284e96
patrickjstefaniak <patrickjstefaniak@gmail.com>
2022-12-08 19:51:21 -0800

    redid front page, added vfx, added artist statement

--- portfolio.js
+++ portfolio.js
@@ -1,13 +1,10 @@
 var currentPhoto = document.getElementById('photop');
-var currentLink = document.getElementById('linkp');
 var currentName = document.getElementById("namep");
 var currentInfo = document.getElementById("infop");
 var currentBody = document.getElementById("bodyp");
 var currentVideo = document.getElementById("videop");
 var portfolioitems = portfoliostuff.items;
 var portfolioDiv = document.getElementById("portfoliod");
-var nextb = document.getElementById("next");
-var previousb = document.getElementById("previous");
 var photoOverlay = document.getElementById("photoOverlay");
 var photoO = document.getElementById("photoO");
 var projectLink = document.getElementById("projectLink");
@@ -16,12 +13,8 @@ var projectLink = document.getElementById("projectLink");
 var currentItem = 0;
 var vimeoLink = "https://player.vimeo.com/video/";
 var imageLink = "images/";
-
 var iframe = document.querySelector('iframe');
 var player = new Vimeo.Player(iframe);
-
-nextb.onclick = function(){startTransition(true)};
-previousb.onclick = function(){startTransition(false)};
 currentPhoto.onclick = function(){openPhotoOverlay()};
 photoOverlay.onclick = function(){closePhotoOverlay()};
 projectLink.onclick = function(){openLink()};
@@ -32,37 +25,12 @@ setNewItem();
 function setNewItem(){
 	currentPhoto.style.opacity = 0;
 	currentVideo.style.opacity = 0;
-	currentLink.style.opacity = 0;
 	currentVideo.style.pointerEvents = "none";
 	currentPhoto.style.pointerEvents = "none";
-	currentLink.style.pointerEvents = "none";
 	currentName.innerHTML = portfolioitems[currentItem].name;
 	currentInfo.innerHTML = portfolioitems[currentItem].info;
 	currentBody.innerHTML = portfolioitems[currentItem].body;
 	setMedia();
-	setTimeout(function(){setTrans(false)}, 500);
-}
-
-function startTransition(next){
-	currentItem = next ? 
-		currentItem + 1 >= portfolioitems.length ? 0 : currentItem + 1 :
-		currentItem - 1 < 0 ? portfolioitems.length - 1 : currentItem - 1;
-	console.log('hi')
-	setTrans(true);
-	player.pause();
-	setTimeout(setNewItem, 500);
-}
-
-function setTrans(off){
-	var value = off ? 0 : 1;
-	if(off){
-		currentPhoto.style.opacity = 0;
-		currentVideo.style.opacity = 0;
-		currentLink.style.opacity = 0;
-	}
-	currentName.style.opacity = value;
-	currentInfo.style.opacity = value;
-	currentBody.style.opacity = value;
 }
 
 
@@ -83,13 +51,6 @@ function setMedia(){
 		currentVideo.style.pointerEvents = "all";
 		setTimeout(function(){currentVideo.style.opacity = 1;}, 500);
 		player.play();
-		break;
-
-		case "link":
-		currentLink.href = item.link;
-		
-		currentLink.style.pointerEvents = "all";
-		setTimeout(function(){currentLink.style.opacity = 1;}, 500);
 		break;
 	}
 }
