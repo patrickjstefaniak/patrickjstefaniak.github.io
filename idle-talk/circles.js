ecd8362448e1b19b12da09688b1af90ca98629c4
patrickjstefaniak <patrickjstefaniak@gmail.com>
2024-12-04 23:08:46 -0800

    updated circles

--- idle-talk/circles.js
+++ idle-talk/circles.js
@@ -1,5 +1,6 @@
 let initcolor = 255/2;
 let rad; 
+let radmax;
 let r = initcolor + 20;
 let g = initcolor - 20;
 let b = initcolor;
@@ -11,13 +12,18 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   background(initcolor);
   noStroke();
+  if(windowWidth > windowHeight){
+    radmax = windowHeight - 50;
+  }else{
+    radmax = windowWidth - 50;
+  }
 }
 
 function draw() {
   //background(initcolor,10);
   fill(r,g,b,5);
   circle(windowWidth/2, windowHeight/2, rad);
-  rad = constrain(rad + random(-1*sizechangemax,sizechangemax),0, windowHeight-50);
+  rad = constrain(rad + random(-1*sizechangemax,sizechangemax),0, radmax);
   r = constrain(r + random(-1*changemax,changemax), 0, 255);
   g = constrain(g + random(-1*changemax,changemax), 0, 255);
   b = constrain(b + random(-1*changemax,changemax), 0, 255);
