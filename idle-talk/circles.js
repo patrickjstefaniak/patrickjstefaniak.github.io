7df78c3e8c14e4e5308f5a70cff9ea275d626397
patrickjstefaniak <patrickjstefaniak@gmail.com>
2024-12-04 23:04:07 -0800

    added circles

---
+++ idle-talk/circles.js
@@ -0,0 +1,29 @@
+let initcolor = 255/2;
+let rad; 
+let r = initcolor + 20;
+let g = initcolor - 20;
+let b = initcolor;
+let changemax = 1;
+let sizechangemax = 20;
+
+function setup() {
+  rad = windowWidth/2;
+  createCanvas(windowWidth, windowHeight);
+  background(initcolor);
+  noStroke();
+}
+
+function draw() {
+  //background(initcolor,10);
+  fill(r,g,b,5);
+  circle(windowWidth/2, windowHeight/2, rad);
+  rad = constrain(rad + random(-1*sizechangemax,sizechangemax),0, windowHeight-50);
+  r = constrain(r + random(-1*changemax,changemax), 0, 255);
+  g = constrain(g + random(-1*changemax,changemax), 0, 255);
+  b = constrain(b + random(-1*changemax,changemax), 0, 255);
+  changemax = constrain(changemax + random(-.1,.1),0,30);
+  sizechangemax = constrain(sizechangemax + random(-.1,.1),0,30);
+  if(random(0,10000) < 2){
+    background(initcolor);
+  }
+}
